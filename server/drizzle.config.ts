b5044d8913a27a2b08478e6681450012224e2df7
Robert Talberg <Robert.talberg@icloud.com>
2026-02-27 10:54:08 +0100

    added connection to datbase

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'drizzle-kit';
+import { config } from 'dotenv';
+
+config({ path: '../../.env' });
 
 export default defineConfig({
   schema: './src/db/schema.ts',
   out: './drizzle',
-  dialect: 'postgres',
+  dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL ?? '',
   },
